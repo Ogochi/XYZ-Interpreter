5a9f65533f1e553ae6fff2b6f7c501cd77cdc4e3
Michał Ołtarzewski <mi.oltarzewski@gmail.com>
2019-04-21 10:53:31 +0200

    Check for expressions

--- StaticCheck.hs
+++ StaticCheck.hs
@@ -69,9 +69,42 @@ checkExp (EString _) = return Str
 checkExp ELitTrue = return Bool
 checkExp ELitFalse = return Bool
 checkExp (ELitInt _) = return Int
-checkExp (EOr exp1 exp2) = do
+checkExp (EOr exp1 exp2) =
+  expOperation exp1 exp2 Bool "OR requires boolean values" Bool
+
+checkExp (EAnd exp1 exp2) =
+  expOperation exp1 exp2 Bool "AND requires boolean values" Bool
+
+checkExp (ERel exp1 _ exp2) =
+  expOperation exp1 exp2 Int "Relational operators requires integer values" Bool
+
+checkExp (EAdd exp1 addOp exp2) = do
+  case addOp of
+    Plus -> do
+      t1 <- checkExp exp1
+      t2 <- checkExp exp2
+      result <- addOperation t1 t2
+      return result
+    Minus -> expOperation exp1 exp2 Int "Substraction requires integer values" Int
+
+checkExp (EMul exp1 _ exp2) =
+  expOperation exp1 exp2 Int "Multiplication requires integer values" Int
+
+checkExp (Neg exp) =
+  expOperation exp (ELitInt 0) Int "Negation requires integer values" Int
+
+checkExp (Not exp) =
+  expOperation exp ELitTrue Bool "NOT requires boolean values" Bool
+
+addOperation :: Type -> Type -> StaticCheckMonad Type
+addOperation Str Str = return Str
+addOperation Int Int = return Int
+addOperation _ _ = throwError $ WrongTypeException "Adding requires integer or string values"
+
+expOperation :: Expr -> Expr -> Type -> String -> Type -> StaticCheckMonad Type
+expOperation exp1 exp2 requiredType exceptionMsg returnType = do
   t1 <- checkExp exp1
   t2 <- checkExp exp2
-  if not (t1 == Bool) || not (t1 == t2)
-    then throwError $ WrongTypeException "OR requires boolean values"
-    else return Bool
+  if not (t1 == requiredType) || not (t1 == t2)
+    then throwError $ WrongTypeException exceptionMsg
+    else return returnType
